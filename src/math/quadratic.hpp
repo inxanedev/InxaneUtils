8b0ef37b5735eae463c8725902429c3894f47946
inxaneninja <inxaneninja@gmail.com>
2020-12-02 08:01:23 +0100

    Added simple error handling capability to quadratic function, returns (0, 0) if sqrt is complex

--- src/math/quadratic.hpp
+++ src/math/quadratic.hpp
@@ -6,9 +6,12 @@ namespace inx {
     namespace math {
         template <typename T>
     	std::tuple<T, T> quadratic(T a, T b, T c) {
-    		T surd = sqrt(pow(b, 2) - 4 * a * c);
+			T under = pow(b, 2) - 4 * a * c;
+			if (under < 0)
+				return std::make_tuple<T, T>(0, 0);
+			T surd = sqrt(under);
 			T denom = 2 * a;
-    		return std::make_tuple<T, T>(
+			return std::make_tuple<T, T>(
     			(-b + surd)/denom,
     			(-b - surd)/denom
     		);
