5fa0b14ef2ab3b5773f07a3ebebd29f683ec22e2
inxaneninja <inxaneninja@gmail.com>
2020-12-06 10:10:20 +0100

    Updated the algorithm to find min/max value from Vote

--- src/data/vote.hpp
+++ src/data/vote.hpp
@@ -2,6 +2,7 @@
 #include <unordered_map>
 #include <vector>
 #include <algorithm>
+#include <functional>
 namespace inx {
     namespace data {
         template <typename T = int32_t> 
@@ -23,35 +24,24 @@ namespace inx {
                 uint32_t get_votes(T&& key) {
                     return m_Data[key];
                 }
-
                 std::vector<T> top() {
                     std::vector<T> result;
-                    uint32_t max = 0;
-                    for (auto& pair : m_Data) {
-                        if (pair.second > max) {
-                            result.clear();
-                            result.push_back(pair.first);
-                            max = pair.second;
-                        }
-                        if (pair.second == max && std::find(result.begin(), result.end(), pair.first) == result.end()) result.push_back(pair.first);
-                    }
+                    uint32_t target = (*std::max_element(m_Data.begin(), m_Data.end(), [](auto& a, auto& b) {
+                        return a.second < b.second;
+                    })).second;
+                    for (auto& pair : m_Data)
+                        if (pair.second == target) result.push_back(pair.first);
                     return result;
                 }
-
                 std::vector<T> bottom() {
                     std::vector<T> result;
-                    uint32_t min = 0;
-                    for (auto& pair : m_Data) {
-                        if (result.size() == 0) {
-                            result.push_back(pair.first);
-                            min = pair.second;
-                        } else {
-                            if (pair.second < min) result.clear();
-                            else if (pair.second == min && std::find(result.begin(), result.end(), pair.first) == result.end()) result.push_back(pair.first);
-                        }
-                    }
+                    uint32_t target = (*std::max_element(m_Data.begin(), m_Data.end(), [](auto& a, auto& b) {
+                        return a.second > b.second;
+                    })).second;
+                    for (auto& pair : m_Data)
+                        if (pair.second == target) result.push_back(pair.first);
                     return result;
                 }
-        };
+         };
     }
 }
